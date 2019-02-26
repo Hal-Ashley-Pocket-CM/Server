485a7cd4b6e211fbe1fee085f7754c271e53ad0d
Ashley <anoelj86@yahoo.com>
2019-02-25 18:33:01 -0700

    routes2 psuedo code routes needed

---
+++ controlers/routes2.js
@@ -0,0 +1,13 @@
+// The purpose of this file is to psuedo code the routes needed to run the case manager application
+
+// login function that hits firebase or some other authentication to allow a case manager to log in this should handle a username/email and password
+
+//function that allows a case manager to update their password this will accept an updated password and change it in the db
+
+//Refresh button function that goes and gets any updated messages and sends them to the front end
+
+//Send Later function that takes a date/time and schedules a time to send a message.
+
+//Send Now function that takes a message string and sends it to a client
+
+//LogOut function for a case manager
